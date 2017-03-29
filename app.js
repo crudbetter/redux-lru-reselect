1f14ee1a9e7d9a679dcac02d0581d053609b53d5
Mike Godfrey <mike@crudbetter.com>
2017-03-29 12:51:09 +0100

    lru-cache, redux, reselect

---
+++ app.js
@@ -0,0 +1,60 @@
+const { createStore } = require('redux')
+const { createSelector } = require('reselect')
+const LRU = require('lru-cache')
+
+const millisInOneDay = 86400000
+
+const initialState = {
+  buffer: new LRU({ max: 300, length: () => 1 })
+}
+
+const UPDATE_BUFFER = 'UPDATE_BUFFER'
+
+function updateBuffer(key) {
+  const now = Date.now()
+
+  console.log(`updateBuffer - ${now}`)
+
+  return {
+    type: UPDATE_BUFFER,
+    key,
+    data: now
+  }
+}
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case UPDATE_BUFFER:
+      const { buffer } = state
+
+      buffer.set(action.key, action.data)
+      
+      return Object.assign({}, state)
+    default:
+      return state
+  }
+}
+
+const bufferSelector = createSelector(
+  [
+    (state, _key) => state.buffer,
+    (_state, key) => key
+  ],
+  (buffer, key) => {
+    return buffer.get(key)
+  }
+)
+
+const store = createStore(reducer)
+
+const unsubscribe = store.subscribe(() => {
+  const state = store.getState()
+  console.log(`bufferSelector - ${bufferSelector(state, 'someKey')}`)
+})
+
+store.dispatch(updateBuffer('someKey'))
+
+setTimeout(() => {
+  store.dispatch(updateBuffer('someKey'))
+  unsubscribe()
+}, 2000)
